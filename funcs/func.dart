6ab173e70ae099110ae077f7c785ceab6d6c2a09
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-11-28 17:07:33 -0300

    Uso de valores default e nomeados

--- funcs/func.dart
+++ funcs/func.dart
@@ -1,9 +1,19 @@
 void main() {
   int x = func(2, 4, c: 54, d: null);
   print("O valor é :  $x");
+  int y = func2(2, 4, 4, 45);
+  print("O valor é :  $y");
 }
 
 int func(int a, int b, {int c = 0, int? d}) {
+  if (d != null) {
+    return a + b + c + d;
+  } else {
+    return a + b + c;
+  }
+}
+
+int func2(int a, int b, [int c = 0, int? d = null]) {
   if (d != null) {
     return a + b + c + d;
   } else {
