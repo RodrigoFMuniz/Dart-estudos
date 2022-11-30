0ff31e764ba8e25ef8219e1e531de01dd60a0a34
Rodrigo <rodrigo.muniz.dev@gmail.com>
2022-11-30 13:37:04 -0300

    Mapas com iteração

--- Mapas/mapas.dart
+++ Mapas/mapas.dart
@@ -8,12 +8,32 @@ void main() {
   print(ddd[21]);
   print(ddd[31]);
   print(ddd[310]);
+  ddd[310] = "Um ddd qq";
 
+  print("**" * 20);
+  print(ddd);
+
+  print("**" * 20);
+  ddd.remove(310);
+  print(ddd);
+
+  print("**" * 20);
   //ddd[310] = null; //Retorna erro a menos que Map<int,String?>
-
+  print(ddd.containsValue("São Paulo"));
+  print(ddd.containsValue("Santos"));
+  print("**" * 20);
   ddd[61] = "Brasilia";
 
+  ddd.addAll({22: "Macaé", 27: "Espitiro Santo"});
+
   ddd.forEach((key, value) => print("key: $key : Valor: $value"));
+  print("**" * 20);
+  ddd.removeWhere((key, value) => value.startsWith("S"));
+  ddd.removeWhere((key, value) => key == 22);
+  ddd.forEach((key, value) => print("key: $key : Valor: $value"));
+  print("**" * 20);
   ddd.keys.forEach((x) => print(x));
+  print("**" * 20);
   ddd.values.forEach((x) => print(x));
+  print("**" * 20);
 }
